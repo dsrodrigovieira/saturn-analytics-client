28439006ac35d155fb0fc685e2a94f8d54f9c159
Rodrigo Vieira <dsrodrigovieira@gmail.com>
2025-01-13 08:32:59 -0300

    comentários no código

--- __index__.py
+++ __index__.py
@@ -1,23 +1,35 @@
 import streamlit as st
 import config
 
+# Dicionário que mapeia as páginas da aplicação
 pages = {
-    "App":[
-        st.Page( page="pages/home.py",
-                 title="Início",
-                 icon=config.ICON_PAGE_HOME
-                ),
-        st.Page( page="pages/envioMetricas.py",
-                 title="Envio de Métricas",
-                 icon=config.ICON_PAGE_ENVIO_METRICAS
-                ),                
-        st.Page( page="pages/consolidacao.py",
-                 title="Consolidação de Indicadores",
-                 icon=config.ICON_PAGE_CONSOLIDACAO
-                ),
+    "App": [
+        # Define a página inicial da aplicação
+        st.Page(
+            page="pages/home.py",
+            title="Início",
+            icon=config.ICON_PAGE_HOME
+        ),
+        # Define a página para envio de métricas
+        st.Page(
+            page="pages/envioMetricas.py",
+            title="Envio de Métricas",
+            icon=config.ICON_PAGE_ENVIO_METRICAS
+        ),
+        # Define a página para consolidação de indicadores
+        st.Page(
+            page="pages/consolidacao.py",
+            title="Consolidação de Indicadores",
+            icon=config.ICON_PAGE_CONSOLIDACAO
+        ),
     ]
 }
 
+# Configura o logotipo da aplicação
 st.logo(config.PATH_LOGO)
+
+# Configura o sistema de navegação da aplicação com base no dicionário de páginas
 pg = st.navigation(pages)
+
+# Executa a página atualmente selecionada na navegação
 pg.run()
